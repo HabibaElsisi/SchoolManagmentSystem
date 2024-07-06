cc6a0c6a827ee88df0332112f860549908982ea8
HabibaElsisi <habibamsisi@gmail.com>
2024-07-06 22:01:59 +0300

    prepares to deploy

--- db/db_Connection.js
+++ db/db_Connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export const dbConnection= ()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/schoolManagmentSystem")
+    mongoose.connect(`mongodb+srv://Bookify:Bookify@cluster0.49gv7u0.mongodb.net/schoolManagementSystem`)
     .then(()=>console.log("mongo Database connected successfully"))
     .catch((err)=>{
         console.log("Database connected error",err)
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ import dotenv from "dotenv"
 import express from 'express'
 import { dbConnection } from './db/db_Connection.js'
 import { bootstrap } from "./src/modules/index.routes.js";
+import cors from "cors"
 const app = express()
 const port = 3000
 app.use(express.json())
+app.use(cors())
 dbConnection()
 dotenv.config()
 bootstrap(app)
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT||port, () => console.log(`Example app listening on port ${port}!`))
